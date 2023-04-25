15773d8ba2ae389d0b4c45b9a36adde5ed5a2aab
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-04-25 13:50:05 -0400

    update lambda code to the code from the aws cloud.

--- api-lambda/model_manager.py
+++ api-lambda/model_manager.py
@@ -1,7 +1,195 @@
 import json
+import asyncio
+import lambda_multiprocessing
 from url_methods import *
 from constants import *
-import lambda_multiprocessing
+
+def get_from_schema(schema, item):
+    """
+    Get the data value asociated with an specific field from a data item
+
+    Params:
+      schema: The schema or field name
+      item: the data record
+    Return:
+        The field value from the record if the field exists in the data item.
+    """
+    if schema is None:
+        return None
+    fields=schema.split(".")
+    while len(fields) > 0:
+        field = fields.pop(0)
+        if field not in item:
+            return None
+        item = item.get(field)
+        if item == "":
+            return None
+    return item
+
+def get_from_table(table_params, field, item):
+    """
+    Get the data value asociated with an specific field from a table inside the
+    model
+
+    Params:
+      table_params:
+        tables: The lookup tables for specific fields in schema
+        lang: The lang for the search
+      field: the nested fields to access the code
+      item: the data record
+    Return:
+        The string corresponding to that table, code, language
+    """
+    tables, lang = table_params
+    code = get_from_schema(field, item)
+    table_name = field.split('.')[0]
+    return tables.get(table_name).get(code).get(lang)
+
+def get_from_array(schema, lookup, item):
+    """
+    Get the data value asociated with an specific field from an array of items
+
+    Params:
+      schema: The schema or field name
+      lookup: the section of schema that defines the rules to extract the value
+              from item
+      item: the data record
+    Return:
+        The field value from the array
+    """
+    item_array = get_from_schema(schema, item)
+    ndx = int(lookup.get("field"))
+    return item_array[ndx]
+
+def get_from_search(field, search_field, contains, return_field, item):
+    """
+    Get the data value asociated with an specific field from an array of items
+    where the item has the searched value 
+
+    Params:
+      field: The field where the list is placed in the data item
+      search_field: the field to search on
+      contains. The value to match the search with
+      return_field. the field containg the returning value
+      item: the data record
+    Return:
+        The 'field' value from the sub-item containing the matching string in
+        the search field.
+    """
+    sub_items = get_from_schema(field, item)
+    for sub_item in sub_items:
+        if contains in get_from_schema(search_field, sub_item):
+            return sub_item.get(return_field)
+    return None
+
+def get_average(schema, index_list, item):
+    """
+    Get the average of a set of values from a numeric array
+
+    Params:
+      schema: The schema or field name
+      indexes: A list with positions of each value for the addition
+      item: the data record
+    Return:
+        The average of the specified values inside the list
+    """
+    total = 0.0
+    addends = len(index_list)
+    if addends < 1:
+        return total
+    item_array = get_from_schema(schema, item)
+    for ndx in index_list:
+        total += item_array[ndx]
+    return total/addends
+
+def function_error():
+    """
+    Return:
+        Error string when the schema doesn't matches
+        any expected type
+    """
+    return ERR_UNEXPECTED_SCHEMA_TYPE
+
+def function_null():
+    """
+    Return:
+        Null value 
+    """
+    return NULL
+
+def get_function_from_schema(schema, item):
+    """
+    Provide the specific function based on the schema section provided
+
+    Params:
+        schema: The section of the schema with the definition of the field
+        item: The data item to be used along with schema to identify which
+              function required
+
+    Return: The function that matches the schema for that specific item field
+    """
+    field = schema.get("field")
+    lookup = schema.get("lookup")
+    if field == "":
+        return function_null
+    if not lookup:
+        return get_from_schema
+    schema_type = lookup.get("type")
+    if schema_type == "table":
+        return get_from_table
+    elif schema_type == "array":
+        return get_from_array
+    elif schema_type == "search":
+        return get_from_search
+    elif schema_type == "average":
+        return get_average
+    elif schema_type == "url":
+        return get_from_url
+    else:
+        return function_error
+
+def get_results(table_params, function_field, item):
+    """
+    Apply the function asociated for each field to the item to
+    extract the return value
+
+    Params:
+      table_params:
+        tables: The lookup tables for specific fields in schema
+        lang: The lang for the search
+      function_field: Contains the function and the section of the schema
+                      with the rules to obtain the value for that field
+      item: the data item to obtain the value from
+    Return: The return value for the piece of data either original or altered
+             and an error string where None or empty means no error
+    """
+    function, item_schema = function_field
+    if "get_from_schema" in function.__name__:
+        return function(item_schema.get("field"), item)
+    elif "get_from_table" in function.__name__:
+        return function(table_params, item_schema.get("field"), item)
+    elif "get_from_array" in function.__name__:
+        field = item_schema.get("field")
+        lookup = item_schema.get("lookup")
+        return function(field, lookup, item)
+    elif "get_from_search" in function.__name__:
+        field = item_schema.get("field")
+        search_field = item_schema.get("lookup").get("search_field")
+        contains = item_schema.get("lookup").get("contains")
+        return_field = item_schema.get("lookup").get("return_field")
+        return function(field,
+                        search_field,
+                        contains,
+                        return_field,
+                        item)
+    elif "get_average" in function.__name__:
+        field = item_schema.get("field")
+        ndx_list = item_schema.get("lookup").get("at")
+        return function(field, ndx_list, item)
+    elif "function_null" in function.__name__:
+        return function()
+    else:
+        return function_error()
 
 def validate_against_schema(value, definition):
     """
@@ -9,12 +197,12 @@ def validate_against_schema(value, definition):
 
     Params:
       value: The data value to be evaluated
-      params: The section of the schema with the definition of the field
+      definition: The specific schema section to validatethe value
 
-    Returns: The return value for the piece of data either original or altered
+    Return: The return value for the piece of data either original or altered
              and an error string where None or empty means no error
     """
-    if value is not None:
+    if value is not None and value != NULL:
         data_type = definition.get("type")
         if data_type == "string":
             if not isinstance(value, str):
@@ -34,6 +222,8 @@ def validate_against_schema(value, definition):
             return val, ""
         elif data_type == "array":
             if not isinstance(value,list):
+                if isinstance(value, int):
+                    value = str(value)
                 value = value.split(",")
             min_items = definition.get("minItems")
             items = definition.get("items")
@@ -71,7 +261,7 @@ def get_data_layer(schema, data):
       schema: The schema with the data structure needed
       data: The raw data read from the service
 
-    Returns: The the section of the schema for the data transformation and
+    Return: The the section of the schema for the data transformation and
              the layer of data where is the required information 
     """
     schema_out = schema.get("lookup").get("out")
@@ -84,30 +274,94 @@ def get_data_layer(schema, data):
     schema_fields_layer = schema_out.get("data")
     return schema_fields_layer, data_fields_layer
 
-def apply_out_api_pool(item_tuple):
+def get_functions(schema, item):
+    """
+    Based on the schema and one item from the load, get the set of 
+    functions to be applied to each field
+
+    Params:
+      schema: The schema (or part of it) that applies to the field
+      item: An item to match the schema with, to identify the function that
+            applies for each field
+
+    Return: the set of functions and schemas to apply on each field
+    """
+    #  in schema from first data item
+    functions_by_field = {}
+    for key in schema:
+        schema_field = schema.get(key)
+        if isinstance(schema_field, dict):
+            schema_function = get_function_from_schema(schema_field, item)
+            functions_by_field[key] = [(schema_function, schema_field)]
+        else:
+            functions_by_field[key] = []
+            for item_schema in schema_field:
+                schema_function = get_function_from_schema(item_schema, item)
+                functions_by_field[key].append((schema_function, item_schema))
+    return functions_by_field
+
+async def apply_service_schema(service,
+                               table_params,
+                               functions_by_field,
+                               data_item):
+    """
+    Extract the required information from each item based on the service model
+
+    Params:
+      service: The name of the service to be added at the begging of each item
+      table_params:
+        tables: The lookup tables for specific fields in schema
+        lang: The lang for the search
+      function_by_field: the list of field functions
+      data_item: The item to be affected by the functions
+
+    Return: A restructured new item matching the output requirements
+    """
+    item = {'key': service}
+    for key in functions_by_field:
+        functions = functions_by_field.get(key)
+        if len(functions) == 1:
+            item[key] = get_results(table_params, functions[0], data_item)
+        else:
+            result_list = []
+            for function_field in functions:
+                result_list.append(get_results(table_params,
+                                               function_field,
+                                               data_item))
+            item[key] = result_list
+    return item
+
+def apply_out_schema(parameters_tuple):
     """
     Validate the data item against the output-api-schema
 
     Params:
-      item_tuple: To be processed in parallel
+      parameters_tuple:
         schema_items: list with all the output items
         schema_required: list with all required items
         item: the item item data to be validated
 
-    Returns: The item affected by the model where either absent default values
+    Return: The item affected by the model where either absent default values
              or data errors are point out 
     """
-    schema_items, schema_required, item = item_tuple
-    for key in schema_items:
+    output_schema, item = parameters_tuple
+    output_fields = output_schema.get("properties")
+    output_required = output_schema.get("required")
+    for key in output_fields:
         value = item.get(key)
         # Validate required parameters
-        if (key in schema_required) and (value is None):
-            value = ERR_ATTRIBUTE_NOT_FOUND
-            item[key] = value
+        if (key in output_required) and (value is None):
+            try:
+                if key == "lat":
+                    item[key] = get_average("bbox", [1, 3], item)
+                if key == "lng":
+                    item[key] = get_average("bbox", [0, 2], item)
+            except:
+                item[key] = ERR_ATTRIBUTE_NOT_FOUND
         else:
             # Validate value against schema
-            key_definition = schema_items.get(key)
-            val, schema_error = validate_against_schema(value, \
+            key_definition = output_fields.get(key)
+            val, schema_error = validate_against_schema(value,
                                                         key_definition)
             if schema_error:
                 item[key] = f"{value} - {schema_error}"
@@ -115,148 +369,45 @@ def apply_out_api_pool(item_tuple):
                 item[key] = val
     return item
 
-def adapt_to_model(service, model, schema_layer, item):
+def items_from_service(service,
+                       table_params,
+                       service_schema,
+                       output_schema,
+                       load):
     """
-    Extract the required information from each item based on the service model
-
-    Params:
-      service: The name of the service to be added at the begging of each item
-      model: The model schema with tables to extract from
-      schema_layer: The layer from the schema to apply as model for the item
-      item: The item to be affected by the model
-
-    Returns: A restructured new item matching the output requirements
-    """
-    list_to_process=[]
-    output_item = {"key": service}
-    # Start each record with 'key:service' to match the out-api-schema
-    for key in schema_layer:
-        schema = schema_layer.get(key)
-        list_to_process.append((key, schema, item, model))
-
-    num =len(list_to_process)
-    with lambda_multiprocessing.Pool(num) as process:
-        items = process.map(apply_schema_by_field, list_to_process)
-    for item in items:
-        output_item.update(item)
-    return output_item
-
-def items_from_service(service, model, schema_items, schema_required, load):
-    """
-    Based on the output schema and api-out schema, returns the formated
+    Based on the output schema and api-out schema, return the formated
     data using multiprocessing to accelerate the task
 
     Params:
       service: The name of the service to put it ahead of the item's data
-      model: The model schema with tables to extract from
+      table_params:
+        tables: The lookup tables for specific fields in schema
+        lang: The lang for the search
       schema_items: the section of the out-api schema to process the data layer
       schema_required: The section of the out-api schema to validate the
-                       prescence of the required field in the data layer
-      load: the section of the item where the data can be extracted from
+                       prescence of 'required' fields in the data layer
+      load: The input set of data items
 
-    Returns: A single instance containing all the
-    schemas, each readable through its own key
+    Return: A set of output items standarized and validated
     """
     list_to_process = []
-    schema = model.get_schema()
-    schema_layer, data_layer = get_data_layer(schema, load)
-    #multiproces for the first function
-    for data_item in data_layer:
-        item = adapt_to_model(service, model, schema_layer, data_item)
+    loads = []
+    #schema = model.get_schema()
+    schema_layer, data_layer = get_data_layer(service_schema, load)
+    # Identify the process by field
+    if len(data_layer) > 0:
+        functions_by_field = get_functions(schema_layer, data_layer[0])
+        # Readjust the item's data based on the service's schema
+        for data_item in data_layer:
+            item = asyncio.run(
+                apply_service_schema(service,
+                                     table_params,
+                                     functions_by_field,
+                                     data_item))
+            # Add the item to the list for the next process
+            list_to_process.append((output_schema, item))
+        num =len(list_to_process)
         # Apply the 'generic' out-api-schema to the item
-        list_to_process.append((schema_items, schema_required, item))
-    num =len(list_to_process)
-    with lambda_multiprocessing.Pool(num) as process:
-        loads = process.map(apply_out_api_pool, list_to_process)
+        with lambda_multiprocessing.Pool(num) as process:
+            loads = process.map(apply_out_schema, list_to_process)
     return loads
-
-def get_from_schema(schema, item):
-    """
-    Get the data value asociated with an specific field from a data item
-
-    Params:
-      schema: The schema or field name
-      item: the data record
-    Returns:
-        The field value from the record if the field exists in the data item.
-    """
-    if schema is None:
-        return None
-    fields=schema.split(".")
-    while len(fields) > 0:
-        field = fields.pop(0)
-        if field not in item:
-            return None
-        item = item.get(field)
-        if item == "":
-            return None
-    return item
-
-def get_from_array(schema, lookup, item):
-    """
-    Get the data value asociated with an specific field from an array of items
-
-    Params:
-      schema: The schema or field name
-      lookup: the section of schema that defines the rules to extract the value
-              from item
-      item: the data record
-    Returns:
-        The field value from the array
-    """
-    item_array = get_from_schema(schema, item)
-    ndx = int(lookup.get("field"))
-    return item_array[ndx]
-
-def get_from_dictionary(schema, item, model):
-    """
-    Based on the schema select and apply the method to use for the data
-    adquisition from the item
-
-    Params:
-      schema: The schema that defines the adquisition rules
-      item: the data record
-      model: The model schema with tables to extract from
-    Returns:
-        The field value from item
-    """
-    field = schema.get("field")
-    lookup = schema.get("lookup")
-    if field == "":
-        return NULL
-    if not lookup:
-        return  get_from_schema(field, item)
-    else:
-        schema_type = lookup.get("type")
-        if schema_type == "table":
-            return  model.get_from_table(field ,item)
-        elif schema_type == "array":
-            return  get_from_array(field, lookup, item)
-        elif schema_type == "url":
-            return  get_from_url(schema, item)
-        else:
-            return  ERR_UNEXPECTED_SCHEMA_TYPE
-
-def apply_schema_by_field(parameters_tuple):
-    """
-    Extract the required information from each item based on the service model
-
-    Params:
-      parameters_tuple: To be processed in parallel
-        key: The key of the field in the model
-        schema: The schema of transformation for that field
-        item: The data field to be obtained based on the schema
-        model: The model schema with tables to extract from
-
-    Returns: A restructured new item matching the output requirements
-    """
-    key, schema, item, model = parameters_tuple
-    if isinstance(schema,dict):
-        return {key: get_from_dictionary(schema, item, model)}
-    elif isinstance(schema,list):
-        list_for_field = []
-        for item_schema in schema:
-            list_for_field.append(get_from_dictionary(item_schema, item, model))
-        return {key: list_for_field}
-    else:
-        return {key: ERR_UNEXPECTED_SCHEMA_TYPE}
--- api-lambda/s3_manager.py
+++ api-lambda/s3_manager.py
@@ -1,16 +1,20 @@
 import boto3
+import csv
 from constants import *
 
 def get_s3_bucket():
     """
     Get the name of the bucket
 
-    Returns: The name of the bucket in S3
+    Return: The name of the bucket in S3
            # This name is just for developping and testing purposes
     """
     return "dev-app-geolocator"
 
 def get_substring(string, start, end):
+    """
+    Return: The string between two substrings
+    """
     return (string.split(start))[1].split(end)[0]
 
 def get_objects(bucket_name):
@@ -19,7 +23,7 @@ def get_objects(bucket_name):
 
     Param:
       bucket_name: The name of the bucket to read from
-    Returns: List of objects in the bucket
+    Return: List of objects in the bucket
     """
     list_obj = []
     try:
@@ -34,32 +38,60 @@ def get_objects(bucket_name):
         logging.error(e)
         return False
 
+def get_tables(bucket_name, tables_path):
+    """
+    Read and return the content in bucket's tables path 
+
+    Param:
+      bucket_name: The name of the bucket to read from
+      table_path: The path to the csv files inside the bucket
+    Return: The content of the csv files transformed into dictionaries
+    """
+    tables = {}
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    for obj in bucket.objects.filter(Prefix=TABLES_PATH):
+        table_path = obj.key
+        if table_path.endswith(CSV):
+            key = get_substring(table_path, TABLES_PATH, CSV)
+            file_body = read_file(bucket_name, table_path)
+            data = file_body.splitlines()
+            records = csv.reader(data)
+            headers = next(records)
+            codes = {}
+            headerCount = len(headers)
+            for eachRecord in records:
+                descr = {}
+                
+                for count in range(1,headerCount):
+                    descr[headers[count]] = eachRecord[count]
+                codes[eachRecord[0]] = descr
+            tables[key] = codes
+    return tables
+
 def get_schemas_paths(bucket_name):
     """
     Get the path to access all the objects in the bucket
 
     Param:
       bucket_name: The name of the bucket to read from
-    Returns: The paths to the objects in the bucket
+    Return: The paths to the objects in the bucket
     """
     apis, services = {}, {}
-    api_starts = 'api/'
-    service_starts = 'services/'
-    ends = '-schema.json'
 
     objects = get_objects(bucket_name)
 
     for item in objects:
         # identification des services
-        if item.endswith(ends):
-            if item.startswith(api_starts):
-                key = get_substring(item, api_starts, ends)
+        if item.endswith(SCHEMA_JSON):
+            if item.startswith(API_PATH):
+                key = get_substring(item, API_PATH, SCHEMA_JSON)
                 apis[key] = item
-            elif item.startswith('services'):
-                key = get_substring(item, service_starts, ends)
+            elif item.startswith(SERVICES_PATH):
+                key = get_substring(item, SERVICES_PATH, SCHEMA_JSON)
                 services[key] = item
             else:
-                raise Exception("unknown item in bucket: "+ item)
+                raise Exception(ERR_UNKNOWN_ITEM_IN_BUCKET + item)
 
     paths = {
         APIS: apis,
@@ -76,7 +108,7 @@ def read_file(bucket, filename):
       bucket: The name of the bucket to read from
       filename: the path to the object
 
-    Returns: The content of the object as string
+    Return: The content of the object as string
     """
     s3 = boto3.resource('s3')
     try:
