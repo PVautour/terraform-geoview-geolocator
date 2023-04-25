0c52635db88bdaba901b9658124b23f0fab28d72
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-04-25 15:08:18 -0400

    BUG FIX: handler now set to the correct value.

--- main.tf
+++ main.tf
@@ -69,9 +69,9 @@ resource "aws_lambda_function" "example" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename         = "lambda_function_payload.zip"
-  function_name    = "geoview-api-geolocator"
+  function_name    = "geolocator-lambda-cf"
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "geolocator-lambda.lambda_handler"
+  handler          = "lambda_function.lambda_handler"
   source_code_hash = data.archive_file.lambda.output_base64sha256
   timeout          = 30
   runtime          = "python3.7"
