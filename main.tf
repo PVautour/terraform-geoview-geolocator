2fc5231b92c088c75434e19d1cf13b141d3bbfd7
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-04-26 11:06:53 -0400

    added method response to allow json response to pass through on valid requests.

--- main.tf
+++ main.tf
@@ -134,3 +134,27 @@ resource "aws_api_gateway_integration" "integration" {
   EOF
   }
 }
+
+resource "aws_api_gateway_integration_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.rest-api.id
+  resource_id = aws_api_gateway_resource.rest-api-resource.id
+  http_method = aws_api_gateway_method.get.http_method
+  status_code = "200"
+
+  response_templates = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.rest-api.id
+  resource_id = aws_api_gateway_resource.rest-api-resource.id
+  http_method = aws_api_gateway_method.get.http_method
+  status_code = "200"
+}
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.rest-api.id
+  resource_id = aws_api_gateway_resource.rest-api-resource.id
+  http_method = aws_api_gateway_method.get.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+}
