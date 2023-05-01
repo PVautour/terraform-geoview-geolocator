ade27932ea98999e45786cbd8caf8bfe2d8d8d6f
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2023-05-01 11:27:14 -0400

    fix requests to nominatim.

--- api-lambda/url_methods.py
+++ api-lambda/url_methods.py
@@ -1,5 +1,5 @@
 import json
-import requests
+from requests import Request, Session
 import asyncio
 
 def get_from_field(field, item):
@@ -109,9 +109,9 @@ def url_request(url, params):
 
     Return: The response from the call.
     """
-    query_response = requests.get(
-        url=url,
-        params=params,
-        timeout=5)
+    s = Session()
+    request = Request('GET', url, params=params)
+    prepared_request = request.prepare()
+    query_response = s.send(prepared_request)
     json_response = query_response.json()
     return json_response
